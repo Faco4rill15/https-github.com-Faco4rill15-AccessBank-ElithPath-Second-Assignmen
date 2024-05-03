78423ef6a87db66995290ca6480375005cb0d342
Faco4rill15 <148912713+Faco4rill15@users.noreply.github.com>
2024-05-03 12:27:22 -0700

    Add files via upload

---
+++ 02 Assignment 2/js/index.js
@@ -0,0 +1,28 @@
+// Here we define our function to calculate length and width of a rectangle
+function calculateArea(length, width){
+    let area = length * width
+    return area;
+}
+// the function is called here
+calculateArea()
+
+// we use console.log() for debugging
+console.log(calculateArea())
+
+// Here we define our function to reverse a  string
+function reverseString(str){
+    return str;
+
+}
+// call of the function here
+reverseString("I am a Programmer")
+console.log(reverseString())
+
+// Here we define our function to check if a number is an even number or not
+function isEven(){
+    let  n = 100
+    return (n % 2 == 0);
+}
+// Function calling
+isEven()
+console.log(isEven())
